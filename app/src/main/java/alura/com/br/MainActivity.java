1874a8ed113569b035d79576f32b778b8b4fe797
Dieinimy Maganha <dieinimy@gmail.com>
2019-12-06 20:46:44 -0300

    Criando textViews

--- app/src/main/java/alura/com/br/MainActivity.java
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -2,17 +2,29 @@ package alura.com.br;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.widget.Toast;
+
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Jose"));
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
